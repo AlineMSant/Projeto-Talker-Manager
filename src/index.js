cd6acb62e337f86c0625baadc3836fefd273bfb1
AlineMSant <alinems4120@gmail.com>
2023-05-17 15:37:57 -0300

    requisito 2 concluido

--- src/index.js
+++ src/index.js
@@ -18,6 +18,17 @@ app.get('/talker', async (req, res) => {
     return res.status(200).json(talkers);
 });
 
+app.get('/talker/:id', async (req, res) => {
+    const talkers = await talkerUtils.readTalker();
+    const talker = talkers.find((obj) => obj.id === Number(req.params.id));
+
+    if (talker) {
+      return res.status(200).json(talker);
+    }
+    
+    return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
